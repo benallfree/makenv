37bf0be87157f8eb22690c264285ab232f0661d7
Ben Allfree <ben@benallfree.com>
2014-06-26 07:00:28 -0700

    Initial commit

---
+++ src/environment.php
@@ -0,0 +1,42 @@
+<?php
+namespace BenAllfree\Makenv;
+
+/*
+Parse the JSON structure from YD_SERVER_CONFIG into individual defines.
+See util/makenv.php for more information.
+*/  
+$fname = __DIR__."/.env.php";
+$data = null;
+if(file_exists($fname))
+{
+  $data = require($fname);
+  $data = $data['APP_CONFIG'];
+  $data = json_decode($data,true);
+}
+if(!$data)
+{
+  $data = json_decode($_SERVER['APP_CONFIG'],true);
+}
+if(!$data)
+{
+  die("Must define APP_CONFIG either in .env.php as a JSON array, or in an environment variable. Use vendor/bin/makenv to make your string.");
+}
+function r_build_config($arr, &$stack=null)
+{
+  if(!$stack) $stack = array();
+  foreach($arr as $k=>$v)
+  {
+    $k = strtoupper($k);
+    $stack[] = $k;
+    if(is_array($v))
+    {
+      r_build_config($v,$stack);
+    } else {
+      $name = join($stack, '_');
+      define('\'.$name, $v);
+//      error_log("$name, $v");
+    }
+    array_pop($stack);
+  }
+}
+r_build_config($data);
